b26304b6bca904b7d0b2a8e665ac13af3b5b0bc6
彭元彬 <592721086@qq.com>
2023-09-07 22:01:04 +0800

    update.

--- com/iokays/start/start.py
+++ com/iokays/start/start.py
@@ -2,9 +2,20 @@
 # -*- coding: UTF-8 -*-
 
 import subprocess
+import platform
 
-subprocess.Popen(r'cmd /k start Snipaste.exe', cwd='D:/App/Snipaste-2.8.3')
-# subprocess.Popen(r'cmd /k start shutdown.cmd', cwd='D:/App/Nacos/nacos-server-2.1.0/bin')
-subprocess.Popen(r'cmd /k start startup.cmd -m standalone', cwd='D:/App/Nacos/nacos-server-2.1.0/bin')
-subprocess.Popen(r'cmd /k start redis-server.exe', cwd='D:/App/Redis')
+sys_platform = platform.platform().lower()
+print(sys_platform)
+
+if "windows" in sys_platform:
+    subprocess.Popen(r'cmd /k start Snipaste.exe', cwd='D:/App/Snipaste-2.8.3')
+    # subprocess.Popen(r'cmd /k start shutdown.cmd', cwd='D:/App/Nacos/nacos-server-2.1.0/bin')
+    subprocess.Popen(r'cmd /k start startup.cmd -m standalone', cwd='D:/App/Nacos/nacos-server-2.1.0/bin')
+    subprocess.Popen(r'cmd /k start redis-server.exe', cwd='D:/App/Redis')
+
+
+
+
+
+
 
