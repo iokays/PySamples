f65392f92e4bfccd59fdcae16322db7b9206b8eb
彭元兵 <pengyb06@vanke.com>
2023-10-16 12:04:24 +0800

    update

---
+++ com/iokays/longman/pronunciation.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+# -*- coding: UTF-8 -*-
+import os.path
+
+import requests
+import requests_cache
+import time
+from bs4 import BeautifulSoup
+
+def download_mp3(word: str):
+    requests_cache.install_cache(backend=requests_cache.SQLiteCache(f'cache/longman_3000.db'))
+    url = 'https://www.ldoceonline.com/dictionary/'
+
+    header = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36 Edg/111.0.1661.41"}
+
+    html = requests.get(url + word, headers=header)
+
+    soup = BeautifulSoup(html.text, 'lxml')
+
+    span = soup.find('span', class_='ldoceEntry Entry')
+
+    mp3_url = span.find('span', class_='speaker brefile fas fa-volume-up hideOnAmp').get('data-src-mp3')
+    print(mp3_url)
+
+    mp3_file_url = 'mp3/' + word + '.mp3'
+
+    if not os.path.exists(mp3_file_url):
+        mp3 = requests.get(mp3_url, headers=header)
+        with open(mp3_file_url, 'wb') as f:
+            f.write(mp3.content)
+            f.close()
+            print('download mp3 success')
+
+
+# 读取longman_3000.txt文件
+with open('longman_3000.txt', 'r') as f:
+    for line in f.readlines():
+        word = line.strip()
+        print(word)
+        download_mp3(word)
+        time.sleep(1)
+
+
+
+
+
+
+
+
+
+
+
+
+
